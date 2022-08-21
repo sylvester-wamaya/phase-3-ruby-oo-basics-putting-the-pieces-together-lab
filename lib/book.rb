0012350d14baf1dd0cd4ddc90ec532995e5885d8
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-08-21 20:24:03 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
-
+class Book
+    attr_reader :title, :turn_page
+    attr_accessor :author, :page_count, :genre
+def initialize(title)
+    @title = title
+end
+def author=(author)
+    @author = author
+end
+def page_count=(page_count)
+    @page_count = page_count
+end
+def genre=(genre)
+    @genre = genre
+end
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
 # Make your shoe class here!
+class Shoe
+attr_reader :brand, :cobble
+attr_accessor :color, :size, :material, :condition
+def initialize(brand)
+    @brand = brand
+end
+def color=(color)
+    @color = color
+end
+def size=(size)
+    @size = size
+end
+def material=(material)
+    @material = material
+end
+def condition=(condition)
+    @condition = condition
+end
+def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+end
+end
